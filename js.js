bd2005347db0fcd98783014633b4c7e096352254
Devlop-er <michael.revellin@gmail.com>
2019-02-12 12:54:06 +0100

    Update

---
+++ js.js
@@ -0,0 +1,28 @@
+
+//  /$$$$$$$$                                         /$$                    
+// | $$_____/                                        |__/                    
+// | $$       /$$   /$$  /$$$$$$   /$$$$$$   /$$$$$$$ /$$  /$$$$$$$  /$$$$$$ 
+// | $$$$$   |  $$ /$$/ /$$__  $$ /$$__  $$ /$$_____/| $$ /$$_____/ /$$__  $$
+// | $$__/    \  $$$$/ | $$$$$$$$| $$  \__/| $$      | $$| $$      | $$$$$$$$
+// | $$        >$$  $$ | $$_____/| $$      | $$      | $$| $$      | $$_____/
+// | $$$$$$$$ /$$/\  $$|  $$$$$$$| $$      |  $$$$$$$| $$|  $$$$$$$|  $$$$$$$
+// |________/|__/  \__/ \_______/|__/       \_______/|__/ \_______/ \_______/
+
+
+// Calculatrice
+
+alert("Voici une calculette, tu va pouvoir additionner deux nombre !");
+
+var Nombre1 = prompt('Quel nombre choisi tu ?');
+
+var Nombre2 = prompt('Choisis un deuxième nombre');
+
+var somme = Number(Nombre1) + Number(Nombre2);
+
+
+alert("Le résultat est : " + somme)
+
+
+
+                                                                          
+                                                                                                                                              
